249171f2d24f1f533f643aecfc1e4eecf735a8db
Flynn Hou <flynnhou7@gmail.com>
2022-03-08 21:24:42 +0800

    feat(layout): style components from mocking q1.jpg

--- src/components/uploader/uploader.tsx
+++ src/components/uploader/uploader.tsx
@@ -2,9 +2,28 @@ import React from 'react';
 
 import styles from './uploader.module.css';
 
+function UploaderHeader() {
+  return (
+    <div className={styles.uploaderHeader}>
+      <div className={styles.uploaderHeader__icon} />
+    </div>
+  );
+}
+
+function UploaderUploadArea() {
+  return (
+    <div className={styles.uploaderUploadArea}>
+      <p>Upload image</p>
+    </div>
+  );
+}
+
 function Uploader() {
   return (
-    <div className={styles.uploader}>uploader</div>
+    <div className={styles.uploader}>
+      <UploaderHeader />
+      <UploaderUploadArea />
+    </div>
   );
 }
 
