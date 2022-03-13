d134c791f385dd3c9f5fabd2b832b17c91c523e5
GreenLu <green.ee02g@g2.nctu.edu.tw>
2022-03-13 08:28:18 +0800

    feat: layout

---
+++ src/components/circle.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const  Circle = () => {
+  return (
+   <div className="circle"></div>
+  );
+}
+
+export default Circle;
---
+++ src/components/playground.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Circle from './circle'
+
+const  Playground = () => {
+  return (
+   <div className="playground">
+       <div className="header">
+        <Circle/>
+       </div>
+       <div className="body">
+           
+       </div>
+      
+   </div>
+  );
+}
+
+export default Playground;
