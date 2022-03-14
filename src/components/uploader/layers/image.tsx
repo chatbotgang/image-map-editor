aafaae4c887622b6502e07ae594d12782ddc660b
Flynn Hou <flynnhou7@gmail.com>
2022-03-14 23:21:46 +0800

    fix(image): eliminate react-hooks/exhaustive-deps warning
    [How]
      Replace hooks with event handler to dispatch uploaded image
    dimension.

--- src/components/uploader/layers/image.tsx
+++ src/components/uploader/layers/image.tsx
@@ -1,17 +1,13 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { SyntheticEvent } from 'react';
 
 import styles from '../uploader.module.css';
 import { UploaderEnum, useUploader } from '../../../reducers';
 
 export const UploaderLayerImage = () => {
   const { uploader, dispatch } = useUploader();
-  const [configured, setConfigured] = useState(false);
-  const imageRef = useRef<HTMLImageElement>(null);
-  useEffect(() => {
-    if (!imageRef?.current || configured) {
-      return;
-    }
-    const { naturalWidth, naturalHeight, width, height } = imageRef.current;
+  const handleImageLoad = (event: SyntheticEvent<HTMLImageElement, Event>) => {
+    const { naturalWidth, naturalHeight, width, height } =
+      event.target as HTMLImageElement;
     dispatch({
       type: UploaderEnum.SetOriginalImageWidth,
       payload: naturalWidth,
@@ -20,30 +16,19 @@ export const UploaderLayerImage = () => {
       type: UploaderEnum.SetOriginalImageHeight,
       payload: naturalHeight,
     });
-    dispatch({
-      type: UploaderEnum.SetStageWidth,
-      payload: width,
-    });
-    dispatch({
-      type: UploaderEnum.SetStageHeight,
-      payload: height,
-    });
-    dispatch({
-      type: UploaderEnum.SetRatio,
-      payload: width / naturalWidth,
-    });
-    console.log({ naturalWidth, naturalHeight, width, height });
-    setConfigured(!!naturalWidth);
-  });
+    dispatch({ type: UploaderEnum.SetStageWidth, payload: width });
+    dispatch({ type: UploaderEnum.SetStageHeight, payload: height });
+    dispatch({ type: UploaderEnum.SetRatio, payload: width / naturalWidth });
+  };
   if (!uploader.originalImageSrc) {
     return null;
   }
   return (
     <img
-      ref={imageRef}
       src={uploader.originalImageSrc}
       alt={uploader.originalImageName}
       draggable={false}
+      onLoad={handleImageLoad}
       className={styles.uploaderLayerImage}
     />
   );
