acec38b53abfbd57ccb284c5af6fb0cedce33f0d
87xie <oscar87xie@gmail.com>
2022-09-23 01:26:49 +0800

    style: format file name to kebab case

---
+++ src/app.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import GlobalStyle from './styles/global-style';
+
+function App() {
+  return (
+    <>
+      <GlobalStyle />
+      <div></div>
+    </>
+  );
+}
+
+export default App;
