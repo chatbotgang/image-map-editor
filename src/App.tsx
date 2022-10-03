86d14aea61521db17286d098a7607af06628cec7
Zong-Rong Huang <zongrong.h886@gmail.com>
2022-10-03 23:57:31 +0800

    feat: add styles to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,29 @@
 import React from 'react';
-
 import ImagePane from './components/ImagePane'
 import DataPane from './components/DataPane'
 
-function App() {
+import styled from 'styled-components'
+
+type AppProps = {
+  className?: string
+}
+
+const AppJSX = ({className}: AppProps) => {
   return (
-    <div className="App">
+    <div className={`App ${className}`}>
      <ImagePane/>
      <DataPane/>
     </div>
   );
 }
+
+const App = styled(AppJSX)`
+  display: flex;
+  justify-content: center;
+  align-items: flex-start;
+  flex-wrap: nowrap;
+  gap: 135px;
+  padding: 50px 20px;
+`
 
 export default App;
