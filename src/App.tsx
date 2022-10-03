2927652ae842e51b5bb9099073c09efe97c85c2e
Zong-Rong Huang <zongrong.h886@gmail.com>
2022-10-03 23:40:30 +0800

    feat: create DataPane

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
 import ImagePane from './components/ImagePane'
+import DataPane from './components/DataPane'
 
 function App() {
   return (
     <div className="App">
      <ImagePane/>
+     <DataPane/>
     </div>
   );
 }
---
+++ src/components/DataPane.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import styled from 'styled-components'
+
+type DataPaneProps = {
+  className?: string
+}
+
+const DataPaneJSX = ({className}: DataPaneProps) => {
+  return (
+    <aside className={className}>
+      <code></code>
+    </aside>
+  )
+}
+
+const DataPane = styled(DataPaneJSX)`
+  width: 548px;
+  height: 703px;
+  border-radius: 5px;
+  background-color: #2B3948;
+`
+
+export default DataPane
+
