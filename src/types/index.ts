0988a8f15968e24684e9b1cee5e9cb2338e69c2d
Zong-Rong Huang <zongrong.h886@gmail.com>
2022-10-07 00:12:12 +0800

    refactor: extract common types for reuse

---
+++ src/types/index.ts
@@ -0,0 +1,12 @@
+export type Object = {
+  [key: string]: any;
+};
+
+export type Rectangle = {
+  id?: string;
+  x: number;
+  y: number;
+  width: number;
+  height: number;
+  isHovered?: boolean;
+};
