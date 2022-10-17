c6d8ff41c2d490f0a50eb39ca39ed622635b4c5f
reJoker <djack0812@gmail.com>
2022-10-17 22:42:40 +0800

    feat(App.tsx): init landing page
    Replace the generated react landing page with an simple dummy page.

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,12 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test("renders two panes", () => {
+    render(<App />);
+    const imagePreviewPaneElement = screen.getByText(/Image preview pane/i);
+    expect(imagePreviewPaneElement).toBeInTheDocument();
+
+    const dataPreviewPaneElement = screen.getByText(/Data preview pane/i);
+    expect(dataPreviewPaneElement).toBeInTheDocument();
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import styled from "styled-components";
+
+const StyledWrapper = styled.div`
+    background-color: #282c34;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+
+    > * {
+        flex-grow: 1;
+    }
+`;
+
+const StyledItem = styled.div`
+    background-color: white;
+    padding: 16px 24px;
+    display: flex;
+    justify-content: center;
+`;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <StyledWrapper>
+            <StyledItem>Image preview pane</StyledItem>
+            <StyledItem>Data preview pane</StyledItem>
+        </StyledWrapper>
+    );
 }
 
 export default App;
