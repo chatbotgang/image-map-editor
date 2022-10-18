ca823abb21f42bdf8a8725ac2164b1cfffb5a0be
reJoker <djack0812@gmail.com>
2022-10-18 10:04:00 +0800

    style(ImagePreviewPane): update layout

--- src/components/ImagePreviewPane.tsx
+++ src/components/ImagePreviewPane.tsx
@@ -1,7 +1,45 @@
 import React, { useState, useEffect } from "react";
+import styled from "styled-components";
 
 import ImageUploader from "./ImageUploader";
 import ImagePreviewer, { Rect } from "./ImagePreviewer";
+
+const StyledWrapper = styled.div`
+    width: 433px;
+    height: 792px;
+
+    display: flex;
+    flex-direction: column;
+    border-radius: 5px;
+    overflow: scroll;
+`;
+
+const Header = styled.div`
+    background-color: #ebf0f3;
+    height: 56px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+
+    padding: 12px 24px;
+
+    > div {
+        background-color: #d3d9de;
+        border-radius: 50%;
+        width: 24px;
+        height: 24px;
+    }
+`;
+
+const Content = styled.div`
+    flex-grow: 1;
+    background-color: #f4f9fa;
+    padding: 24px 24px;
+
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`;
 
 interface ImagePreviewPaneProps {
     selectedRects: Rect[];
@@ -24,11 +62,11 @@ const ImagePreviewPane = (props: ImagePreviewPaneProps) => {
     }, [imageData]);
 
     return (
-        <div>
-            <div>
-                <div>icon</div>
-            </div>
-            <div>
+        <StyledWrapper>
+            <Header>
+                <div></div>
+            </Header>
+            <Content>
                 {imageData ? (
                     <ImagePreviewer
                         selectedRects={selectedRects}
@@ -38,8 +76,8 @@ const ImagePreviewPane = (props: ImagePreviewPaneProps) => {
                 ) : (
                     <ImageUploader setImageData={setImageData}></ImageUploader>
                 )}
-            </div>
-        </div>
+            </Content>
+        </StyledWrapper>
     );
 };
 
